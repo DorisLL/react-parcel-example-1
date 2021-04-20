26f0ec1fcce57703e895c6de6bd457d292bf46a2
Doris <suyinl@uw.edu>
2021-04-20 10:26:03 -0700

    0413 test changefile structure

--- src/App.js
+++ src/App.js
@@ -9,21 +9,31 @@ const App = () => {
     // https://developer.mozilla.org/en-US/docs/Web/SVG
     // return 里面： <rect x = {200} y = {100} width="10" height="10" fill="rgb(200,200,200)"/>
     // 在一级里，别用颜色
-    return <svg style={{
-        border: "1px solid lightgrey", 
-        width: viewHeight,
-        height: viewHeight }}>
-    <circle cx={circleCX} cy={50} r="5"/>
-    <rect x = {200} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
-    <rect x = {212} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
-    <rect x = {224} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
-    <rect x = {236} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
-    <rect x = {248} y = {100} width="10" height="10" />
-    <line x1="20" y1={viewHeight - 20} x2={150} y2={100} stroke="black"> </line>
-    <text x="20" y="35" style={{font: "italic 16px sans-serif"}}>
-        Price history of 100 randomly selected Pokemon cards
-    </text>
-</svg>
+    
+// // 0406 LAB EXAMPLE
+//     return <svg style={{
+//         border: "1px solid lightgrey", 
+//         width: viewHeight,
+//         height: viewHeight }}>
+//     <circle cx={circleCX} cy={50} r="5"/>
+//     <rect x = {200} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
+//     <rect x = {212} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
+//     <rect x = {224} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
+//     <rect x = {236} y = {100} width="10" height="10" fill="rgb(230,230,230)"/>
+//     <rect x = {248} y = {100} width="10" height="10" />
+//     <line x1="20" y1={viewHeight - 20} x2={150} y2={100} stroke="black"> </line>
+//     <text x="20" y="35" style={{font: "italic 16px sans-serif"}}>
+//         Price history of 100 randomly selected Pokemon cards
+//     </text>
+// </svg> 
+
+    return (
+        <div>
+            <p>from App rerender</p>
+            <p>now for a worldview demo</p>
+            <p>class on April 13</p>
+        </div>
+    );
 };
 
 export default App;
